f6299a011da50e850d9154700d33a9bfbcaf9199
maroo <mgoothan.psnl@gmail.com>
2022-10-03 11:25:09 +0630

    setup main in app service. close #9

--- app/app-services/cmd/front_api/main.go
+++ app/app-services/cmd/front_api/main.go
@@ -1,5 +1,49 @@
 package main
 
-import _ "nsq-demoset/app/app-services/conf"
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	logger "nsq-demoset/app/_applib"
+	"nsq-demoset/app/app-services/cmd/front_api/handler"
+	_ "nsq-demoset/app/app-services/conf"
+	_ds "nsq-demoset/app/nsq-services/ds"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
-func main() {}
+func main() {
+	// load datasource
+	ds := _ds.NewDataSource()
+
+	// server
+	router := gin.Default()
+	h := handler.NewHandler(&handler.HConfig{
+		R:  router,
+		DS: ds,
+	})
+	h.Register()
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", os.Getenv("APP_PORT")),
+		Handler: h.R,
+	}
+
+	go func() {
+		logger.Sugar.Info("Server started listening on port: ", os.Getenv("APP_PORT"))
+		if err := server.ListenAndServe(); err != nil {
+			logger.Sugar.Error("Failed to initialized server: ", err.Error())
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-c
+
+	// shutdown close
+	if err := server.Shutdown(context.Background()); err != nil {
+		logger.Sugar.Error("Failed to shutdown server: ", err.Error())
+	}
+}
